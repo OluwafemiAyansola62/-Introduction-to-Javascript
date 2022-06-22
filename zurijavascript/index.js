e997a9e9df4e5401551a861fd3ea0429d8511619
OluwafemiAyansola62 <105220576+OluwafemiAyansola62@users.noreply.github.com>
2022-06-22 15:37:36 +0100

    Add files via upload

---
+++ zurijavascript/index.js
@@ -0,0 +1,11 @@
+let fname = "Sewa";
+let country = "UK";
+let H = 6;
+
+//console.log(name+ " is my name, i am from the, " + country , "i am " + H , "Feet tall.");
+
+console.log(`my name is, ${fname}  i am from the, ${country}  and my height is ${H} feet `)
+
+
+
+
